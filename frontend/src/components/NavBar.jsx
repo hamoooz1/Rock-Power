f60d633899478adcc0905f8db5b963c533c747bf
Hamza Kaderi <kaderihamza4@gmail.com>
2024-04-22 23:28:31 -0500

    created navbar and imported logo

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import '../styles/NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <h1>
+        
+      </h1>
+    </div>
+  );
+}
+
+export default NavBar;

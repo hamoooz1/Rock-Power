911c7d7545bd178623bfdafc019424cdecccd61b
Hamza Kaderi <kaderihamza4@gmail.com>
2024-04-23 21:07:55 -0500

    navbar frontend completed

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 import '../styles/NavBar.css';
+import logo from '../logo.png'
 
 function NavBar() {
+  let items = ['Home', 'Our Programs', 'About Us', 'FAQ', 'User Policy', 'Contact Us'];
+
   return (
     <div className="NavBar">
-      <h1>
-        
-      </h1>
+      <img src={logo} className="logoItem"/>
+      {items.map((item, index) => (
+        <h1 className="navItem" key={index}>{item}</h1>
+      ))}
     </div>
   );
 }

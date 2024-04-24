30f853dd8b1221e36107f51e8f685963db840060
Hamza Kaderi <kaderihamza4@gmail.com>
2024-04-23 21:19:09 -0500

    created contact navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,12 +6,25 @@ function NavBar() {
   let items = ['Home', 'Our Programs', 'About Us', 'FAQ', 'User Policy', 'Contact Us'];
 
   return (
-    <div className="NavBar">
-      <img src={logo} className="logoItem"/>
-      {items.map((item, index) => (
-        <h1 className="navItem" key={index}>{item}</h1>
-      ))}
-    </div>
+    <>
+      <div className="contact-info">
+        <h4>
+          Call: +1 437 439 5865
+        </h4>
+        <h3 className="contact-seperator">
+          |
+        </h3>
+        <h4>
+          Email: info@bantued.com
+        </h4>
+      </div>
+      <div className="NavBar">
+      < img src={logo} className="logoItem"/>
+        {items.map((item, index) => (
+          <h1 className="navItem" key={index}>{item}</h1>
+        ))}
+      </div>
+    </>
   );
 }
 

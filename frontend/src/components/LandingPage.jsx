b6d98f506723490a48db7f3c12ce03c1932c2bfa
macadanj-coder <joshryanmac@hotmail.com>
2024-04-26 09:05:42 -0400

    Updated landing page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../styles/LandingPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const LandingPage = () => {
   return (
     <div className="LandingPage">
       <div className='hero-container'>
-        <h1>EMPOWERING AFRICA'S FUTURE WITH AI LEARNING TECHNOLOGIES</h1>
-        <button>Discover our Programs</button>
+        <p className="cta-text">EMPOWERING AFRICA'S FUTURE WITH AI LEARNING TECHNOLOGIES</p>
+        <button className="cta-button">
+          <span>
+            Discover our Programs 
+          </span> 
+          <FontAwesomeIcon icon={faChevronRight} />          
+        </button>
       </div>
     </div>
   );
